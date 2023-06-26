05c43dff9650a04aa72c688a7c790307210255c3
Jadranka S <jaca.stupar@gmail.com>
2023-06-26 15:33:27 +0200

    Update port to 82 in Dockerfile.

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -2,6 +2,6 @@ public class Test {
 
     public static void main(String[] args) {
         System.out.println("Hello Docker!");
-        System.out.println("Hello Docker2!");
+        //System.out.println("Hello Docker2!");
     }
 }
